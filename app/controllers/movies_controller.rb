c8de23a522f7fcbc74d2a835c458e1aece5c21f2
ribrciti <rbciti@msn.com>
2015-04-23 18:11:40 -0700

    Added paperclip to upload images

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -57,6 +57,6 @@ class MoviesController < ApplicationController
     end
 
     def movie_params
-      params.require(:movie).permit(:title, :description, :director, :movie_length, :rating)
+      params.require(:movie).permit(:title, :description, :director, :movie_length, :rating, :image)
     end
 end
